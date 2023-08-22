861ba707ee6c0de9b61263388487f1a9c9be4250
luzifago <andradelogystic@gmail.com>
2023-08-22 12:32:34 -0500

    cambios

---
+++ js/starbucks.js
@@ -0,0 +1,8 @@
+alert('hola mundo')
+function imgSlider(anything){
+    document.querySelector('.starbucks').src = anything;
+}
+function changecircleColor(color){
+    const circle =document.querySelector('.circle');
+    circle.style.background=color;       
+}
